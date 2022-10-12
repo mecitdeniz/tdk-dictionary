699250b6dd9406b410e5d6fbf375144b305eb360
Mecit Deniz <mecit.deniz@extrawatts.co>
2022-10-12 19:18:38 +0300

    search bar design

---
+++ app/src/main/java/com/mecitdeniz/tdkdictionary/feature_dictionary/presentation/home/components/Search.kt
@@ -0,0 +1,63 @@
+package com.mecitdeniz.tdkdictionary.feature_dictionary.presentation.home.components
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun Search(
+    spacialChars: List<String> = emptyList()
+) {
+
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        TextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            shape = RoundedCornerShape(14.dp),
+            value = "Sözlükte Ara", onValueChange = {},
+            colors = TextFieldDefaults.textFieldColors(
+                backgroundColor = Color.White,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                cursorColor = Color.Black
+            ),
+            trailingIcon = {
+                IconButton(onClick = { }) {
+                    Icon(
+                        imageVector = Icons.Filled.Search,
+                        contentDescription = null
+                    )
+                }
+            },
+        )
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 16.dp, end = 16.dp, bottom = 16.dp),
+        ) {
+
+            spacialChars.forEachIndexed { index, spacialChar ->
+                SpecialCharButton(char = spacialChar)
+                if (index < spacialChars.size - 1) {
+                    Spacer(modifier = Modifier.width(8.dp))
+                }
+            }
+
+        }
+    }
+
+}
+
